69f10dc2205e828b83b7a5cb945fe8b731f78c65
adhiti2006 <prag2013>
2021-02-27 08:35:52 +0530

    [Updated port and end point url]

--- src/main/groovy/com/gcp/inventoryservice/controller/InventoryController.groovy
+++ src/main/groovy/com/gcp/inventoryservice/controller/InventoryController.groovy
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController()
+@RequestMapping('/api/v1/inventory')
 class InventoryController {
 
     @Autowired
     InventoryService inventoryService
 
-    @RequestMapping('inventory/{id}')
+    @RequestMapping('/{id}')
     @GetMapping
     boolean isSkuSellable(@PathVariable String id) {
         return inventoryService.isSkuSellable(id)
     }
 
-    @RequestMapping('inventory/{id}/{condition}')
+    @RequestMapping('/{id}/{condition}')
     @GetMapping
     boolean isOpenBoxSkuSellable(@PathVariable String id, @PathVariable String condition) {
         return inventoryService.isSkuSellable(id, condition)
